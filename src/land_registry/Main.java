5f1c5198467213e95f4beedafa1f5359b8f0f0fa
the_usik <me@usik.space>
2020-05-27 20:37:55 +0300

    first commit

--- src/land_registry/Main.java
+++ src/land_registry/Main.java
@@ -27,7 +27,6 @@ public class Main extends Application {
 
     public void close() {
         database.closeConnection();
-        System.out.println("test");
     }
 
     public Stage getPrimaryStage() {
